b488983e6ec60b30eeb4c876729cf6b658709559
Gokul Raj k p <gokulraj.k.p123@gmail.com>
2021-05-24 20:59:06 +0530

    room databse connection

--- app/src/main/java/com/example/jsonapiapp/MainActivity.java
+++ app/src/main/java/com/example/jsonapiapp/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.jsonapiapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.view.Menu;
-import android.widget.CompoundButton;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,6 +21,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.jsonapiapp.room.DatabaseClient;
+import com.example.jsonapiapp.room.Recipe;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import org.json.JSONArray;
@@ -23,6 +30,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     AdapterList adapterList;
     ArrayList<ModelList> modelList = new ArrayList<>();
     String imageDataUrl = "https://picsum.photos/v2/list?page=1&limit=20";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +54,44 @@ public class MainActivity extends AppCompatActivity {
         homeRv.setLayoutManager(new LinearLayoutManager(this));
         homeRv.setAdapter(adapterList);
 
-        getImageJsonData();
+        try {
+            ConnectivityManager coonmgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = coonmgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnectedOrConnecting() ) {
+                Toast.makeText(this, "from server", Toast.LENGTH_SHORT).show();
+                fetchfromServer();
+            } else {
+                Toast.makeText(this, "from room", Toast.LENGTH_SHORT).show();
+
+                fetchfromRoom();
+            }
+
+        }catch (Exception e){
+            System.out.println("Check connectivity Exception"+e.getMessage());
+
+
+        }
+
+
     }
 
-    private void getImageJsonData() {
+    private void fetchfromRoom() {
+        Toast.makeText(this, "room connected", Toast.LENGTH_SHORT).show();
+        List<Recipe> recipeList = DatabaseClient.getInstance(MainActivity.this).getAppDatabase().recipeDao().getAllUsers();
+        for (Recipe recipe: recipeList){
+            ModelList repo = new ModelList(recipe.getId(),
+                    recipe.getWidth(),
+                    recipe.getHeight(),
+                    recipe.getAuthor(),
+                    recipe.getImageUrl(),
+                    recipe.getDownloadUrl());
+            modelList.add(repo);
+
+            adapterList.notifyDataSetChanged();
+        }
+    }
+
+    private void fetchfromServer() {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, imageDataUrl,
                 new Response.Listener<String>() {
                     @Override
@@ -64,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
                                 itemSingle.setAuthor(jsonList.getString("author"));
                                 itemSingle.setDownloadUrl(jsonList.getString("download_url"));
                                 modelList.add(itemSingle);
+                                SaveTask();
+                                Toast.makeText(MainActivity.this, "accesing", Toast.LENGTH_SHORT).show();
                             }
                             adapterList.notifyDataSetChanged();
 
@@ -93,6 +139,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 changeListViewType(isChecked);
+                changeicon();
             }
         });
         return true;
@@ -109,6 +156,46 @@ public class MainActivity extends AppCompatActivity {
             homeRv.setLayoutManager(new LinearLayoutManager(this));
             homeRv.setAdapter(adapterList);
         }
+
+    }
+    public void changeicon(){
+
+
+    }
+
+    public void SaveTask(){
+        class SaveTask extends AsyncTask<Void,Void ,Void>{
+            @Override
+            protected Void doInBackground( Void...voids) {
+                    for (int i =0;i<modelList.size();i++){
+                        Recipe recipe = new Recipe();
+                        recipe.setId(modelList.get(i).getId());
+                        recipe.setWidth(modelList.get(i).getWidth());
+                        recipe.setHeight(modelList.get(i).getHeight());
+                        recipe.setAuthor(modelList.get(i).getAuthor());
+                        recipe.setImageUrl(modelList.get(i).getImageUrl());
+                        recipe.setDownloadUrl(modelList.get(i).getDownloadUrl());
+
+                        Log.d("chkauthor",":"+i+modelList.get(0).author);
+                       DatabaseClient.getInstance(getApplicationContext()).getAppDatabase().recipeDao().insert(recipe);
+                    }
+
+
+
+                return null;
+
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(MainActivity.this, "saved", Toast.LENGTH_SHORT).show();
+            }
+        }
+        SaveTask st = new SaveTask();
+        st.execute();
+
+
 
     }
 
